183ab3bd834dab5448b4cf80d009f99e3e56220d
F-ca7 <627955292@qq.com>
2022-08-02 17:13:54 +0800

    refactor BaseExportWorker: make produceData a common method

--- batch-tool/src/main/java/worker/export/BaseExportWorker.java
+++ batch-tool/src/main/java/worker/export/BaseExportWorker.java
@@ -18,20 +18,33 @@ package worker.export;
 
 import model.config.CompressMode;
 import model.config.FileFormat;
+import model.config.GlobalVar;
 import model.config.QuoteEncloseMode;
 import model.db.FieldMetaInfo;
 import model.db.TableFieldMetaInfo;
 import model.db.TableTopology;
+import model.mask.AbstractDataMasker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.DataSourceUtil;
 import util.FileUtil;
+import util.IOUtil;
 
 import javax.sql.DataSource;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public abstract class BaseExportWorker implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(BaseExportWorker.class);
+
     protected final DataSource druid;
     protected final TableTopology topology;
     protected final TableFieldMetaInfo tableFieldMetaInfo;
@@ -43,6 +56,10 @@ public abstract class BaseExportWorker implements Runnable {
     protected FileFormat fileFormat;
 
     protected final List<Boolean> isStringTypeList;
+
+    protected Map<Integer, AbstractDataMasker> columnDataMasker;
+    protected ByteArrayOutputStream os;
+    protected int bufferedRowNum = 0;       // 已经缓存的行数
 
     protected BaseExportWorker(DataSource druid, TableTopology topology,
                                TableFieldMetaInfo tableFieldMetaInfo,
@@ -82,11 +99,66 @@ public abstract class BaseExportWorker implements Runnable {
         this.fileFormat = fileFormat;
     }
 
+    protected void produceData() {
+        String sql = getExportSql();
+
+        try (Connection conn = druid.getConnection();
+            Statement stmt = DataSourceUtil.createStreamingStatement(conn);
+            ResultSet resultSet = stmt.executeQuery(sql)) {
+
+            logger.info("{} 开始执行导出", topology);
+
+            byte[] value;
+            int colNum = resultSet.getMetaData().getColumnCount();
+            this.os = new ByteArrayOutputStream(colNum * 16);
+            while (resultSet.next()) {
+                for (int i = 1; i < colNum; i++) {
+                    value = resultSet.getBytes(i);
+                    writeFieldValue(os, value, i - 1);
+                    // 附加分隔符
+                    os.write(separator);
+                }
+                value = resultSet.getBytes(colNum);
+                writeFieldValue(os, value, colNum - 1);
+                // 附加换行符
+                os.write(FileUtil.SYS_NEW_LINE_BYTE);
+                bufferedRowNum++;
+
+                if (bufferedRowNum == GlobalVar.EMIT_BATCH_SIZE) {
+                    emitBatchData();
+                    os.reset();
+                    bufferedRowNum = 0;
+                }
+            }
+            if (bufferedRowNum != 0) {
+                // 最后剩余的元组
+                dealWithRemainData();
+                os.reset();
+            }
+            afterProduceData();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            logger.error("{} 导出发生错误: {}", topology, e.getMessage());
+        } finally {
+            IOUtil.close(os);
+        }
+    }
+
+    protected void afterProduceData() {
+    }
+
+    protected abstract void emitBatchData();
+
+    protected abstract void dealWithRemainData();
+
+    protected abstract String getExportSql();
+
     /**
      * 根据引号模式来写入字段值
-     * todo 字段值本身包含引号 还需要对引号重新转义
+     * @param columnIdx 从 0 开始
      */
-    protected void writeFieldValue(ByteArrayOutputStream os, byte[] value, boolean isStringType) throws IOException {
+    protected void writeFieldValue(ByteArrayOutputStream os, byte[] value, int columnIdx) throws IOException {
+        boolean isStringType = isStringTypeList.get(columnIdx);
         switch (quoteEncloseMode) {
         case NONE:
             FileUtil.writeToByteArrayStream(os, value);
@@ -115,4 +187,7 @@ public abstract class BaseExportWorker implements Runnable {
         this.compressMode = compressMode;
     }
 
+    public void setColumnDataMasker(Map<Integer, AbstractDataMasker> columnDataMasker) {
+        this.columnDataMasker = columnDataMasker;
+    }
 }
